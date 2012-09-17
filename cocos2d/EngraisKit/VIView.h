bf77d1ebf4d6fd4b62057d20b00ea4edb37a9dee
Leix Snake <xiaoming.liu@eintsoft.com>
2012-09-17 18:19:37 +0800

    working on.

---
+++ cocos2d/EngraisKit/VIView.h
@@ -0,0 +1,13 @@
+//
+//  VIView.h
+//  VUEngine
+//
+//  Created by LeixSnake on 9/17/12.
+//
+//
+
+#import <GLKit/GLKit.h>
+
+@interface VIView : GLKView
+
+@end
---
+++ cocos2d/EngraisKit/VIView.m
@@ -0,0 +1,31 @@
+//
+//  VIView.m
+//  VUEngine
+//
+//  Created by LeixSnake on 9/17/12.
+//
+//
+
+#import "VIView.h"
+
+@implementation VIView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
